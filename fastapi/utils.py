253ce475daa0e31bb7a98aedf0550655a2642a76
Ilya Goldobin <yxtagold.iliy@gmail.com>
2024-12-24 16:45:28 +0300

    feat: textloader

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -1,14 +1,21 @@
 import os
 
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_core.documents import Document
 
 
 def load_documents(data_folder_path: str) -> list[Document]:
-    pdfs = [f for f in os.listdir(data_folder_path) if f.endswith(".pdf")]
+    files = [
+        f
+        for f in os.listdir(data_folder_path)
+        if f.endswith(".pdf") or f.endswith(".txt")
+    ]
     documents = []
-    for pdf in pdfs:
-        loader = PyPDFLoader(os.path.join(data_folder_path, pdf))
+    for file in files:
+        if file.endswith(".pdf"):
+            loader = PyPDFLoader(os.path.join(data_folder_path, file))
+        else:
+            loader = TextLoader(os.path.join(data_folder_path, file))
         documents.extend(loader.load())
 
     return documents
